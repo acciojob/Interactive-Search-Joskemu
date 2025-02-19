4070240808a3beb03ad171fb6aac3d33a46001c1
Joskemu <vadhelkemu098@gmail.com>
2025-02-19 21:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const searchDiv = document.querySelector('.search');
+const searchInput = document.querySelector('.input');
+const searchButton = document.querySelector('.btn');
+
+searchButton.addEventListener('click', function() {
+  searchDiv.classList.add('active');  // Add the 'active' class to change styles
+  searchInput.focus();  // Focus on the input field when button is clicked
+});
